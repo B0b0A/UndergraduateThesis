4498c044ca22d01d7602089e8d090dcf1ef08cf0
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-20 00:05:54 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
 #include "bme280.h"
+#include "bluetooth.h"
+#include "wifi.h"
 
+void init_NVS(){
+    esp_err_t ret;
+
+    // Initialize NVS
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+}
 
 void delay_sec(int time)
 {
     vTaskDelay(time * 1000 / portTICK_PERIOD_MS);
 }
 
+// does single measurement as of now
+
 void app_main()
 {
+	init_NVS();
+	init_WiFi();
+	init_blufi();
     bme280_config_t my_bme280 = {
             .sda_pin = GPIO_NUM_19,
             .scl_pin = GPIO_NUM_18,
             .i2c_interface = I2C_NUM_1
     };
-	
-    bme280_measurement_t my_measurement;
-	
-    if(bme280_init(&my_bme280) == 0)
+   if(bme280_init(&my_bme280) == 0)
 	   printf("Error, no connection to sensor");
+   else{
 
-    for (;;)
-    {
+        bme280_measurement_t my_measurement;
         my_measurement = bme280_make_measurement(&my_bme280);
         bme280_print_measurement(&my_measurement);
 
-        delay_sec(5);
-    }
+   }
 }
