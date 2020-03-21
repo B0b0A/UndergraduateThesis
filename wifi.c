9a7929177d29ee10e1a4d5f0e3558fcd2897ce97
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-21 22:49:39 +0100

    Update wifi.c

--- wifi.c
+++ wifi.c
@@ -126,10 +126,7 @@ void init_WiFi()
     ESP_ERROR_CHECK( esp_event_loop_init(net_event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_FLASH) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK( esp_wifi_start() );
 }
-
-
-
