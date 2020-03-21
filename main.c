cd3271daf7b316308a81fea3fe7a27dba2249b76
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-21 22:48:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,18 +11,17 @@
 #include "bluetooth.h"
 #include "wifi.h"
 #include "timers.h"
-#include "main.h"
 
-	// Config for BME280 sensor
-    bme280_config_t my_bme280 = {
-            .sda_pin = GPIO_NUM_19,
-            .scl_pin = GPIO_NUM_18,
-            .i2c_interface = I2C_NUM_1
-    };
-    // Initialize NVS (Non-violatile storage) for the Wi-Fi
+bme280_config_t my_bme280 = {
+        .sda_pin = GPIO_NUM_19,
+        .scl_pin = GPIO_NUM_18,
+        .i2c_interface = I2C_NUM_1
+};
+
 void init_NVS(){
     esp_err_t ret;
 
+    // Initialize NVS
     ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
@@ -36,17 +35,39 @@ void delay_sec(int time)
     vTaskDelay(time * 1000 / portTICK_PERIOD_MS);
 }
 
+static void main_task(void *arg){
+	while(1){
+	bme280_measurement_t my_measurement;
+	my_measurement = bme280_make_measurement(&my_bme280);
+    bme280_print_measurement(&my_measurement);
+    delay_sec(5);
+	}
+}
+
+static void timer_bt_task(void *arg){
+	while(1)
+	{
+	bool evt = 0;
+	xQueueReceive(timer_queue, &evt, portMAX_DELAY);
+	if (evt)
+		{
+		deinit_blufi();
+		deinitTimer(TIMER_GROUP_0, TIMER_0);
+		}
+	}
+}
+
 void app_main()
 {
-	bme280_measurement_t my_measurement;
-	timer_idx_t timer_idx = TIMER_0;
+	timer_queue = xQueueCreate(1, sizeof(bool));
+
 	init_NVS();
 	init_WiFi();
 	init_blufi();
-	tg0_timer_init(timer_idx, WITH_RELOAD, TIMER_INTERVAL1_SEC);
 	bme280_init(&my_bme280);
-    while(1)
-    {
-        xQueueReceive(timer_queue, &my_measurement, portMAX_DELAY);
-    	bme280_print_measurement(&my_measurement);
+	initTimerGroup0(TIMER_0, TEST_WITH_RELOAD, TIMER_INTERVAL0_SEC);
+
+	xTaskCreate(main_task, "main_task", 2048, NULL, 5, NULL);
+	xTaskCreate(timer_bt_task, "timer_bt_task", 2048, NULL, 5, NULL);
+
 }
