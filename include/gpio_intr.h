f258fc3af34154c0d31b4fcef91d27f2dea0e184
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-23 01:38:58 +0100

    Add files via upload

---
+++ include/gpio_intr.h
@@ -0,0 +1,33 @@
+/*
+ * gpio_intr.h
+ *
+ *  Created on: Mar 22, 2020
+ *      Author: Bojan Aleksovski
+ */
+
+#ifndef MAIN_INCLUDE_GPIO_INTR_H_
+#define MAIN_INCLUDE_GPIO_INTR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "driver/gpio.h"
+
+#define GPIO_BT_BTN     5
+#define GPIO_BT_BTN_MASK (1ULL<<GPIO_BT_BTN)
+#define ESP_INTR_FLAG_DEFAULT 0
+
+extern xQueueHandle gpio_evt_queue;
+extern bool bt_on;
+
+void init_bt_button();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_INCLUDE_GPIO_INTR_H_ */
