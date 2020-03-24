ce1715faf8673e12c0a23e7d3d02048e62c4714c
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-24 01:07:56 +0100

    Create tasks.c

---
+++ srcs/tasks.c
@@ -0,0 +1,54 @@
+/*
+ * tasks.c
+ *
+ *  Created on: Mar 24, 2020
+ *      Author: Bojan Aleksovski
+ */
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "bme280.h"
+#include "bluetooth.h"
+#include "wifi.h"
+#include "timers.h"
+#include "gpio_intr.h"
+#include "tasks.h"
+#include "misc.h"
+
+void main_task(void *arg){
+	while(1){
+	my_measurement = bme280_make_measurement(&my_bme280);
+    bme280_print_measurement(&my_measurement);
+    delay_sec(500);
+	}
+}
+
+void timer_turn_off_bt_task(void *arg){
+	while(1){
+	bool evt = 0;
+	xQueueReceive(timer_queue, &evt, portMAX_DELAY);
+	if(evt){
+		deinit_blufi();
+		deinitTimer(TIMER_GROUP_0, TIMER_0);
+		bt_on = 0;
+		}
+	}
+}
+
+void bt_button_pressed_task(void* arg){
+    while(1){
+        xQueueReceive(gpio_evt_queue, &bt_on, portMAX_DELAY);
+        if(bt_on){
+        	init_blufi();
+        	initTimerGroup0(TIMER_0, TEST_WITH_RELOAD, TIMER_INTERVAL0_SEC);
+        }
+    }
+}
+
+void http_send_data_task(void* arg){
+    while(1){
+    	init_http_client();
+    	post_data(prepare_data());
+    	delay_sec(300);
+        }
+    }
