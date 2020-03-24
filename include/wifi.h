4d7493471b40e153235add3c3cb8b22ea4e6ea35
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-24 01:12:30 +0100

    Update wifi.h

--- include/wifi.h
+++ include/wifi.h
@@ -12,22 +12,13 @@
 extern "C" {
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
+#include "main.h"
 #include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_bt.h"
-#include "esp_blufi_api.h"
-#include "esp_bt_defs.h"
-#include "esp_gap_ble_api.h"
-#include "esp_bt_main.h"
-#include "esp_bt_device.h"
+#include "esp_http_client.h"
+#include "esp_tls.h"
+
+#define WIFI_LIST_NUM   10
+#define WEBSITE "http://website12341234.000webhostapp.com/index.php?"
 
 extern wifi_config_t sta_config;
 extern wifi_config_t ap_config;
@@ -38,6 +29,8 @@ extern uint8_t gl_sta_ssid[32];
 extern int gl_sta_ssid_len;
 
 void init_WiFi();
+void init_http_client();
+void post_data(char *data);
 
 #ifdef __cplusplus
 }
