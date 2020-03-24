acc02d5226266ddfd0894bdff7fd5357d998ebc5
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-24 01:11:34 +0100

    Create misc.h

---
+++ include/misc.h
@@ -0,0 +1,32 @@
+/*
+ * misc.h
+ *
+ *  Created on: Mar 24, 2020
+ *      Author: Bojan Aleksovski
+ */
+
+#ifndef MAIN_INCLUDE_MISC_H_
+#define MAIN_INCLUDE_MISC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+#include "nvs_flash.h"
+
+extern bme280_config_t my_bme280;
+extern bme280_measurement_t my_measurement;
+
+char *float_to_string(float temp);
+char *concat(const char *s1, const char *s2);
+char *prepare_data();
+void init_NVS();
+void delay_sec(int time);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_INCLUDE_MISC_H_ */
