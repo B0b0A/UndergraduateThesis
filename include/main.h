bacbc8ddec9baa081d34735d1b0ca697b1ec4e43
B0b0A <33010318+B0b0A@users.noreply.github.com>
2020-03-24 01:11:19 +0100

    Create main.h

---
+++ include/main.h
@@ -0,0 +1,40 @@
+/*
+ * main.h
+ *
+ *  Created on: Mar 20, 2020
+ *      Author: Bojan Aleksovski
+ */
+
+#ifndef MAIN_INCLUDE_MAIN_H_
+#define MAIN_INCLUDE_MAIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_types.h"
+#include "esp_log.h"
+
+#include "bme280.h"
+#include "bluetooth.h"
+#include "gpio_intr.h"
+#include "wifi.h"
+#include "timers.h"
+#include "tasks.h"
+#include "misc.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_INCLUDE_MAIN_H_ */
